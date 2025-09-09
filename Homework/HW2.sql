038bce446e741f9ccefe452c4179287b9eaa1eaf
Parker <panzerchong@gmail.com>
2025-09-09 12:29:36 -0700

    move to folder

--- Homework/HW2.sql
+++ Homework/HW2.sql
@@ -1,78 +1,137 @@
+USE AdventureWorks2019
+
 --1.How many products can you find in the Production.Product table?
+SELECT COUNT(*) from Production.Product
+--Total is 504 products
 
 
 --2.Write a query that retrieves the number of products in the Production.Product table that are included in a subcategory. 
 --The rows that have NULL in column ProductSubcategoryID are considered to not be a part of any subcategory.
-
+SELECT COUNT(*) from Production.Product
+WHERE ProductSubcategoryID is NOT NULL
 
 
 --3.Count how many products belong to each product subcategory.
-	Write a query that displays the result with two columns:
-
-	ProductSubcategoryID (the subcategory ID)， CountedProducts (the number of products in that subcategory).
-
+-- Write a query that displays the result with two columns:
+-- ProductSubcategoryID (the subcategory ID)， CountedProducts (the number of products in that subcategory).
+SELECT ProductSubcategoryID,COUNT(*) AS CountedProducts
+FROM Production.Product
+WHERE ProductSubcategoryID is NOT NULL
+GROUP BY ProductSubcategoryID
 
 
 --4.How many products that do not have a product subcategory.
-
+SELECT COUNT(*) from Production.Product
+WHERE ProductSubcategoryID is NULL
+--There are 209 products don't have a product subcategory
 
 
 --5.Write a query to list the sum of products quantity in the Production.ProductInventory table.
-
+SELECT SUM(Quantity)
+FROM Production.ProductInventory
 
 
 --6.Write a query to list the sum of products in the Production.ProductInventory table 
 --and LocationID set to 40 and limit the result to include just summarized quantities less than 100.
 
 
+SELECT ProductID,SUM(Quantity) as Quantity
+FROM Production.ProductInventory
+WHERE LocationID=40
+GROUP BY ProductID
+HAVING SUM(Quantity) <100
+
 
 --7. Write a query to list the sum of products with the shelf information in the Production.ProductInventory 
 --table and LocationID set to 40 and limit the result to include just summarized quantities less than 100
 
-
+SELECT ProductID,SUM(Quantity) AS Quantity,Shelf
+FROM Production.ProductInventory
+where LocationID=40
+GROUP BY ProductID,Shelf
+HAVING SUM(Quantity)<100
 
 
 --8.Write the query to list the average quantity for products where column LocationID has the value of 10 from the table Production.ProductInventory table.
-
+SELECT AVG(Quantity) AS AverageQuantity
+FROM Production.ProductInventory
+WHERE LocationID = 10
 
 
 --9.Write query  to see the average quantity  of  products by shelf  from the table Production.ProductInventory
-
+SELECT shelf,AVG(quantity)
+FROM Production.ProductInventory
+GROUP BY Shelf
+ORDER BY Shelf ASC
 
 
 --10.Write query  to see the average quantity  of  products by shelf excluding rows that has the value of N/A in the column Shelf from the table Production.ProductInventory
-
+SELECT shelf,AVG(quantity)
+FROM Production.ProductInventory
+WHERE shelf != 'N/A'
+GROUP BY Shelf
+ORDER BY Shelf ASC
 
 
 --11.List the members (rows) and average list price in the Production.Product table. This should be grouped independently over the Color and the Class column. 
 --Exclude the rows where Color or Class are null.
-
-
+SELECT Color,Class,AVG(ListPrice) AS AverageListPrice
+FROM Production.Product
+where Color is NOT NULL AND Class is NOT NULL
+GROUP BY Color,Class
 
 
 --12.Write a query that lists the country and province names from person. CountryRegion and person. StateProvince tables. 
 --Join them and produce a result set similar to the following
-
+SELECT c.name AS Country,s.Name AS Province
+FROM person.CountryRegion c
+INNER JOIN Person.StateProvince s
+on c.CountryRegionCode=s.CountryRegionCode
 
 
 --13.Write a query that lists the country and province names from person. CountryRegion and person. 
 --StateProvince tables and list the countries filter them by Germany and Canada. Join them and produce a result set similar to the following.
+SELECT c.name AS Country,s.Name AS Province
+FROM person.CountryRegion c
+INNER JOIN Person.StateProvince s
+on c.CountryRegionCode=s.CountryRegionCode
+WHERE c.Name = 'Germany' OR c.Name = 'Canada'
+ORDER BY c.Name,s.Name
 
 
-
-
+USE Northwind
 -- Using Northwnd Database: (Use aliases for all the Joins)
 --14.List all Products that has been sold at least once in last 25 years.
-
-
+SELECT p.ProductName,od.OrderID,o.OrderDate
+FROM Products AS p
+INNER JOIN [Order Details] AS od
+on p.ProductID=od.ProductID
+INNER JOIN Orders AS o
+ON o.OrderID=od.OrderID
+WHERE o.OrderDate >= DATEADD(YEAR,-25,GETDATE())
 
 --15.List top 5 locations (Zip Code) where the products sold most.
-
+SELECT TOP(5) c.PostalCode AS ZipCode,SUM(od.Quantity) AS quantities
+FROM Customers as c
+INNER JOIN Orders as o
+ON c.CustomerID=o.CustomerID
+INNER JOIN [Order Details] AS od
+ON od.OrderID=o.OrderID
+WHERE c.PostalCode is NOT NULL
+GROUP BY c.PostalCode
+ORDER BY quantities DESC
 
 
 --16.List top 5 locations (Zip Code) where the products sold most in last 25 years.
-
-
+SELECT TOP(5) c.PostalCode AS ZipCode,SUM(od.Quantity) AS quantities
+FROM Customers as c
+INNER JOIN Orders as o
+ON c.CustomerID=o.CustomerID
+INNER JOIN [Order Details] AS od
+ON od.OrderID=o.OrderID
+WHERE c.PostalCode is NOT NULL AND o.OrderDate >= DATEADD(YEAR,-25,GETDATE())
+GROUP BY c.PostalCode
+ORDER BY quantities DESC
 
 
 --17.List all city names and number of customers in that city.    
